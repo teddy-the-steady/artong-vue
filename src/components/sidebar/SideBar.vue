7cbdfcdfdb23f3bb679c062cee9f826192034efd
teddy <teddy.jeon@speedwagon.io>
2020-03-22 11:12:45 +0900

    components > contents > header / 1차 design 완료

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="sidebar">
     <div class="sidebar__items">
-      <div>H</div>
-      <div>T</div>
-      <div>F</div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   </nav>
 </template>
@@ -16,13 +16,14 @@ export default {
 
 <style scoped>
 .sidebar {
+  overflow-y: auto;
+  background-color: var(--lightgray);
   position: fixed;
   left: 0;
-  bottom: 0;
   top: var(--head-height, 50px);
-  background-color: var(--lightgray);
+  height: 100vh;
+  z-index: 999;
   width: 60px;
-  /* z-index: 2030; */
 }
 
 .sidebar__items {
