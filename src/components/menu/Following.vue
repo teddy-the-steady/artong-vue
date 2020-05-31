652c8aef3f8c41f60a2b87fa08fb06cded775387
teddy <teddy.jeon@speedwagon.io>
2020-05-31 10:06:46 +0900

    contentbox,list / menu폴더 추가

---
+++ src/components/menu/Following.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Following Contents</h1>
+    <contents-list></contents-list>
+  </div>
+</template>
+
+<script>
+import ContentsList from '../content/ContentsList'
+export default {
+  name: 'Following',
+  components: {
+    ContentsList
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+h1 {
+  color: $artong-black;
+}
+</style>
---
+++ src/components/menu/Trending.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Trending Contents</h1>
+    <contents-list></contents-list>
+  </div>
+</template>
+
+<script>
+import ContentsList from '../content/ContentsList'
+export default {
+  name: 'Trending',
+  components: {
+    ContentsList
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+h1 {
+  color: $artong-black;
+}
+</style>
