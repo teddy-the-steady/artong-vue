b84ee9fb4dd0c66a1a432de60904392264eddf6b
teddy <hohoworld11@gmail.com>
2020-10-18 15:52:17 +0900

    ContentLayer 분리

---
+++ src/components/content/ContentLayer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="contentLayer" v-bind:class="{active: isActive}" @click="onLayerClick()">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentLayer',
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    onLayerClick() {
+      this.isActive = !this.isActive
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.contentLayer {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  border-radius: 20px;
+  background-color: $artong-black;
+  opacity: 0;
+  cursor: pointer;
+  &.active {
+    opacity: .3;
+  }
+}
+</style>
