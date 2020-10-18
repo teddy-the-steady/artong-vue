21794911c737f26cf8ba5fd741e824abaf10e4ca
taeho_jeon <superduper8989@gmail.com>
2020-10-18 14:37:08 +0900

    CenterContainer내 contentLayer 추가

--- src/components/content/CenterContainer.vue
+++ src/components/content/CenterContainer.vue
@@ -3,6 +3,7 @@
     <div v-if="image" class="center">
       <!-- <img :src="image.url" alt=""/> -->
       <img :src="require(`../../assets/images/art${image.url}.jpg`)" alt=""/>
+      <div class="contentLayer" v-bind:class="{active: isActive}" @click="onLayerClick()"></div>
     </div>
   </div>
 </template>
@@ -15,6 +16,16 @@ export default {
       type: Object,
       default: null
     }
+  },
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    onLayerClick() {
+      this.isActive = !this.isActive
+    }
   }
 }
 </script>
@@ -23,15 +34,30 @@ export default {
 @import '../../assets/scss/variables';
 
 .center {
-  display: inline-block;
+  display: inline-flex;
   margin: 0.5rem;
   width: 200%;
+  position: relative;
 
   img {
     width: 100%;
     height: auto;
     cursor: pointer;
     border-radius: 20px;
+  }
+
+  .contentLayer {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    border-radius: 20px;
+    background-color: $artong-black;
+    opacity: 0;
+    cursor: pointer;
+    &.active {
+      opacity: .3;
+    }
   }
 }
 </style>
