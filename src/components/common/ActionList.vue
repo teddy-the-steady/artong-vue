0749d9ca46dd8b53bd535ceb103c9807f54558a6
teddy <teddy.jeon@speedwagon.io>
2020-11-07 18:00:08 +0900

    ContentLayer에 ArtistProfile, ActionList 추가

---
+++ src/components/common/ActionList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <img src="../../assets/images/heart.png" alt="">
+      </li>
+      <li>
+        <img src="../../assets/images/comment.png" alt="">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionList'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+ul {
+  display: flex;
+  justify-content: space-around;
+  margin: 0;
+  padding: 0;
+  li {
+    list-style-type: none;
+    img {
+      width: 30px;
+    }
+  }
+}
+
+</style>
