e1203159592ad25fea1c28804eae616354a5e6cf
teddy <hohoworld11@gmail.com>
2020-11-15 22:34:15 +0900

    comment추가 spinner 임시 추가

--- src/components/content/CenterContainer.vue
+++ src/components/content/CenterContainer.vue
@@ -3,7 +3,7 @@
     <div v-if="image" class="center">
       <!-- <img :src="image.url" alt=""/> -->
       <img :src="require(`../../assets/images/art${image.url}.jpg`)" alt=""/>
-      <content-layer :image="image"></content-layer>
+      <content-layer :image="image" class="content-layer"></content-layer>
     </div>
   </div>
 </template>
@@ -33,6 +33,7 @@ export default {
   margin: 0.5rem;
   width: 200%;
   position: relative;
+  user-select: none;
 
   img {
     width: 100%;
@@ -40,5 +41,11 @@ export default {
     cursor: pointer;
     border-radius: 20px;
   }
+}
+
+.content-layer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
 </style>
