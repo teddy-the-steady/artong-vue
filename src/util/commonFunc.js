3b0b22c5b560a137d432ba6b4da9f6f462bb552d
teddy <hohoworld11@gmail.com>
2021-08-14 22:25:27 +0900

    add level in image path

--- src/util/commonFunc.js
+++ src/util/commonFunc.js
@@ -1,18 +1,17 @@
 const parseS3Path = function(path) {
   path = path.split('/')
   return {
-    bucket: path[0],
-    level: path[1],
-    username: path[2],
-    type: path[3],
-    file: path[4]
+    level: path[0],
+    username: path[1],
+    type: path[2],
+    file: path[3]
   }
 }
 
 const setLocalStorageCurrentUserProfilePic = function(key) {
   const currentUser = JSON.parse(localStorage.getItem('current-user'))
   const path = parseS3Path(currentUser.profile.profile_pic)
-  currentUser.profile.profile_pic = `${path[0]}/${path[1]}/${key}`
+  currentUser.profile.profile_pic = `${path.level}/${key}`
   localStorage.setItem('current-user', JSON.stringify(currentUser))
   return currentUser
 }
