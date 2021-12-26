1cf5ff07f4ec7f6ff44fa95b203caf70413164d4
taeho_jeon <superduper8989@gmail.com>
2021-12-26 15:02:46 +0900

    checkAuth 간소화 - amplify 자동 처리

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -11,29 +11,10 @@ const checkAuth = function() {
     return new Promise((resolve, reject) => {
       Auth.currentSession()
         .then((session) => {
-          var accessTokenExpire = session.getAccessToken().getExpiration()
-          var refreshToken = session.getRefreshToken()
-          var currentTimeSeconds = Math.round(+new Date() / 1000)
-          if (accessTokenExpire < currentTimeSeconds) {
-            Auth.currentAuthenticatedUser()
-              .then((res) => {
-                res.refreshSession(refreshToken, (err, data) => {
-                  if (err) {
-                    console.log(err)
-                    Auth.signOut()
-                  } else {
-                    config.headers.Authorization = data.getAccessToken().getJwtToken() 
-                    resolve(config)
-                  }
-                })
-              })
-          } else {
-            config.headers.Authorization = session.getAccessToken().getJwtToken()
-            resolve(config)
-          }
+          config.headers.Authorization = session.getAccessToken().getJwtToken()
+          resolve(config)
         })
         .catch(() => {
-          // No logged-in user: don't set auth header
           reject(config)
         })
     })
