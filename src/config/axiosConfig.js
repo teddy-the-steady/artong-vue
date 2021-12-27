4f2b0c9c5a5d6c72929bbe80810ee0f70a309629
teddy <hohoworld11@gmail.com>
2021-12-27 13:04:38 +0900

    viewport min/max-scale=1 added

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -6,7 +6,7 @@ import { Auth } from 'aws-amplify'
 axios.defaults.baseURL = API_STAGE
 // axios.defaults.baseURL = API_LOCAL
 
-const checkSession = function() {
+const checkSession = function() { // TODO] currentSession 호출 없이 accessToken 만료(현재 1시간)되면 자동 refresh가 안되는 현상? 테스트 필요. 1시간에 한번씩 수동 refresh 돌려야할지도?
   axios.interceptors.request.use(function(config) {
     return new Promise((resolve) => {
       Auth.currentSession()
