9183526fb896c955255cf69d8f111669a4baa230
teddy <hohoworld11@gmail.com>
2022-02-13 00:08:19 +0900

    constants/url.js removed, url .env에서 제공

--- src/constants/url.js
+++
@@ -1,3 +0,0 @@
-export const API_STAGE = 'https://6tz1h3qch8.execute-api.ap-northeast-2.amazonaws.com/stage/artong/v1'
-export const API_LOCAL = 'http://localhost:3000/stage/artong/v1'
-export const CLOUDFRONT_URL = 'https://images.4rtong.com'
