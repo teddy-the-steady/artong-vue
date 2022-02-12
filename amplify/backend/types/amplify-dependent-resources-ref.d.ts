c40ba6703e2d9a2ebcad00d2648c11c3d206d1f0
teddy <hohoworld11@gmail.com>
2022-02-12 17:25:36 +0900

    amplify reset

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "artong905365dc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "artongStage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
