696ecf3b48e4102ecfdb935ca852d57a10ae34e6
teddy <hohoworld11@gmail.com>
2022-04-15 23:34:16 +0900

    comment frame position

--- src/components/contentList/CenterContainer.vue
+++ src/components/contentList/CenterContainer.vue
@@ -7,14 +7,14 @@
         <img v-else :src="require(`../../assets/images/art${image.url}.jpg`)" alt=""/>
         <content-layer :image="image" class="content-layer" :isPropCommentActive="isCommentActive" @handle-comment-click="handleCommentClick"></content-layer>
       </div>
-      <div class="comment" v-if="isCommentActive">
-        <div class="spinner" :class="{active: isSpinnerActive}"></div>
-        <ul class="comment-list" :class="{active: isCommentListActive}">
-          <li>user1: 훌륭한 작품이네요!</li>
-          <li>user2: 잘봤습니다.</li>
-          <li>user3: Good work</li>
-        </ul>
-      </div>
+    </div>
+    <div class="comment" v-if="isCommentActive">
+      <div class="spinner" :class="{active: isSpinnerActive}"></div>
+      <ul class="comment-list" :class="{active: isCommentListActive}">
+        <li>user1: 훌륭한 작품이네요!</li>
+        <li>user2: 잘봤습니다.</li>
+        <li>user3: Good work</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -78,49 +78,49 @@ export default {
       height: 100%;
     }
   }
+}
 
-  .comment {
-    display: block;
-    position: relative;
-    width: 100%;
-    min-height: 57px;
-    background: $artong-white;
-    box-shadow: 1px 1px .5em $lightgray;
-    border-radius: 20px;
+.comment {
+  display: block;
+  position: relative;
+  width: 100%;
+  min-height: 57px;
+  background: $artong-white;
+  box-shadow: 1px 1px .5em $lightgray;
+  border-radius: 20px;
 
-    .comment-list {
-      display: none;
+  .comment-list {
+    display: none;
 
-      &.active {
-        display: block;
-        text-align: left;
-        margin: 0;
-      }
+    &.active {
+      display: block;
+      text-align: left;
+      margin: 0;
     }
+  }
 
-    .spinner {
-      display: none;
+  .spinner {
+    display: none;
 
-      &.active {
-        display: block;
-        position: relative;
-        top: 9px;
-        height: 30px;
-        width: 30px;
-        margin: 0px auto;
-        animation: rotation .6s infinite linear;
-        border-left: 6px solid rgba(0,174,239,.15);
-        border-right: 6px solid rgba(0,174,239,.15);
-        border-bottom: 6px solid rgba(0,174,239,.15);
-        border-top: 6px solid rgba(0,174,239,.8);
-        border-radius: 100%;
-      }
+    &.active {
+      display: block;
+      position: relative;
+      top: 9px;
+      height: 30px;
+      width: 30px;
+      margin: 0px auto;
+      animation: rotation .6s infinite linear;
+      border-left: 6px solid rgba(0,174,239,.15);
+      border-right: 6px solid rgba(0,174,239,.15);
+      border-bottom: 6px solid rgba(0,174,239,.15);
+      border-top: 6px solid rgba(0,174,239,.8);
+      border-radius: 100%;
     }
+  }
 
-    @keyframes rotation {
-      from {transform: rotate(0deg);}
-      to {transform: rotate(359deg);}
-    }
+  @keyframes rotation {
+    from {transform: rotate(0deg);}
+    to {transform: rotate(359deg);}
   }
 }
 
