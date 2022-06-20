0852b261294178b1d49a8cf4c552a73e5ab07416
teddy <teddy.jeon@speedwagon.io>
2022-06-20 20:25:24 +0900

    amplify env checkout prod --restore

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "artong905365dc": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "storage": {
-        "artongStage": {
+        "artongProd": {
             "BucketName": "string",
             "Region": "string"
         }
