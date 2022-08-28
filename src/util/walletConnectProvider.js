f7d31bce8d707a554af4a5958657b377d3eaf478
taeho_jeon <superduper8989@gmail.com>
2022-08-28 12:07:23 +0900

    rainbow -> trust wallet

--- src/util/walletConnectProvider.js
+++ src/util/walletConnectProvider.js
@@ -7,7 +7,7 @@ class Provider {
       qrcodeModalOptions: {
         mobileLinks: [
           'metamask',
-          'rainbow',
+          'trust',
         ]
       }
     })
@@ -19,7 +19,7 @@ class Provider {
       qrcodeModalOptions: {
         mobileLinks: [
           'metamask',
-          'rainbow',
+          'trust',
         ]
       }
     })
