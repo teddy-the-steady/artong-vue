2906e274ef9a56d9da78fa38bc38c61bbbc6c72e
taeho_jeon <superduper8989@gmail.com>
2022-09-07 14:25:13 +0900

    CreateProject ing..

--- src/components/menu/CreateProject.vue
+++ src/components/menu/CreateProject.vue
@@ -2,15 +2,15 @@
   <div>
     <div>
       <span>Name</span>
-      <input :model="name" placeholder="name">
+      <input v-model="name" placeholder="name">
     </div>
     <div>
       <span>Symbol</span>
-      <input :model="symbol" placeholder="symbol">
+      <input v-model="symbol" placeholder="symbol">
     </div>
     <div>
       <span>Max amount</span>
-      <input :model="maxAmount" placeholder="maxAmount">
+      <input v-model="maxAmount" placeholder="maxAmount">
     </div>
     <button @click="createProject">CREATE PROJECT</button>
   </div>
@@ -26,20 +26,31 @@ export default {
     return {
       name: '',
       symbol: '',
-      maxAmount: null
+      maxAmount: 0
     }
   },
   methods: {
     async createProject() {
+      console.log(this.name, this.symbol, this.maxAmount)
       const signer = await getSigner()
       const contract = new ethers.Contract(FACTORY, FACTORY_ABI, signer)
-      const data = await contract.createNFTContract(
+      const tx = await contract.createNFTContract(
         this.name,
         this.symbol,
         this.maxAmount,
         1
       )
-      console.log('data:',data)
+      const res = await tx.wait()
+      console.log('res:',res)
+      res.events.map(evt => {
+        if (
+          evt.topics[0] ===
+          '0x2d49c67975aadd2d389580b368cfff5b49965b0bd5da33c144922ce01e7a4d7b'
+        ) {
+          const address = ethers.utils.hexDataSlice(evt.data, 44)
+          console.log('nft address:', address)
+        }
+      })
     }
   }
 }
