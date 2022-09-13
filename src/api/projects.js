799985ed59fc6231cc9da29ec9e94150445870d2
teddy <hohoworld11@gmail.com>
2022-09-13 17:29:12 +0900

    added CreatingProject, redirect after CreateProject

--- src/api/projects.js
+++ src/api/projects.js
@@ -3,7 +3,12 @@ import instance from './index';
 const postProject = async function(body) {
   return await instance.post('/projects', body)
 }
+
+const patchProject = async function(txHash, body) {
+  return await instance.patchProject(`/projects/${txHash}`, body)
+}
  
 export {
   postProject,
+  patchProject,
 }
