8678e88137d3f392d02b054b8b44882b5310d14f
taeho_jeon <superduper8989@gmail.com>
2022-09-15 17:39:15 +0900

    added getProjectWhileUpdatingCreatedPendingOne, polling until CREATED in CreatingProject, AUTH_SIGN_IN_AND_UP parameter payload and added principalId

--- src/components/projects/CreatingProject.vue
+++ src/components/projects/CreatingProject.vue
@@ -5,8 +5,41 @@
 </template>
 
 <script>
+import { getProjectWhileUpdatingCreatedPendingOne } from '../../api/projects'
 export default {
-  name: 'CreatingProject'
+  name: 'CreatingProject',
+  data() {
+    return {
+      txHash: this.$router.currentRoute.query.txHash
+    }
+  },
+  methods: {
+    wait(timeToDelay) {
+      return new Promise((resolve) => setTimeout(resolve, timeToDelay))
+    },
+    async getProjectWhileUpdatingCreatedPendingOne() {
+      for (;;) {
+        const result = getProjectWhileUpdatingCreatedPendingOne(this.txHash)
+        if (result.data && result.data.status === 'CREATED') {
+          break
+        }
+        if (this.$router.currentRoute.name !== 'CreatingProject') {
+          break
+        }
+        await this.wait(3000)
+      }
+    }
+  },
+  async mounted() {
+    await this.getProjectWhileUpdatingCreatedPendingOne()
+  },
+  watch: {
+    async $route(val) {
+      if (val.name === 'CreatingProject') {
+        await this.getProjectWhileUpdatingCreatedPendingOne()
+      }
+    }
+  }
 }
 </script>
 
