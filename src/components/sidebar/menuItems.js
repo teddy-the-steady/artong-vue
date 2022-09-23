f72c1faa558f52127a44f7d994ece40d88727d7a
taeho_jeon <superduper8989@gmail.com>
2022-09-23 14:02:15 +0900

    FE flow fixing

--- src/components/sidebar/menuItems.js
+++ src/components/sidebar/menuItems.js
@@ -31,18 +31,13 @@ export default [
     title: 'Menu'
   },
   {
-    href: '/',
-    title: 'Home',
-    icon: 'fa fa-home'
-  },
-  {
     href: '/projects',
     title: 'Projects',
     icon: 'fa fa-folder-open'
   },
   {
-    href: '/projects/123',
-    title: 'Project 123',
+    href: '/artworks',
+    title: 'Tokens',
     icon: 'fa fa-image'
   },
   {
@@ -55,84 +50,84 @@ export default [
     title: 'CreateProject',
     icon: 'fa fa-hammer'
   },
-  {
-    header: true,
-    title: 'Example'
-  },
-  {
-    href: '/disabled',
-    title: 'Disabled page',
-    icon: 'fa fa-lock',
-    disabled: true
-  },
-  {
-    href: '/badge',
-    title: 'Badge',
-    icon: 'fa fa-cog',
-    badge: {
-      text: 'new',
-      class: 'art--badge_default'
-    }
-  },
-  {
-    href: '/page1',
-    title: 'Dropdown Page',
-    icon: 'fa fa-list-ul',
-    child: [
-      {
-        href: '/page1/sub-page-1',
-        title: 'Sub Page 01',
-        icon: 'fa fa-file-alt'
-      },
-      {
-        href: '/page1/sub-page-2',
-        title: 'Sub Page 02',
-        icon: 'fa fa-file-alt'
-      }
-    ]
-  },
-  {
-    href: '/page2',
-    title: 'Multiple Level',
-    icon: 'fa fa-list-alt',
-    child: [
-      {
-        href: '/page2/sub-page-1',
-        title: 'page'
-      },
-      {
-        title: 'Level 2 ',
-        child: [
-          {
-            href: '/page2/sub-page-1/sub-sub-page-1',
-            title: 'page'
-          },
-          {
-            href: '/page2/sub-page-1/sub-sub-page-2',
-            title: 'Page'
-          }
-        ]
-      },
-      {
-        href: '/page2/sub-page-2',
-        title: 'Page'
-      },
-      {
-        title: 'Another Level 2',
-        child: [
-          {
-            title: 'Level 3',
-            child: [
-              {
-                title: 'Page'
-              },
-              {
-                title: 'Page'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+  // {
+  //   header: true,
+  //   title: 'Example'
+  // },
+  // {
+  //   href: '/disabled',
+  //   title: 'Disabled page',
+  //   icon: 'fa fa-lock',
+  //   disabled: true
+  // },
+  // {
+  //   href: '/badge',
+  //   title: 'Badge',
+  //   icon: 'fa fa-cog',
+  //   badge: {
+  //     text: 'new',
+  //     class: 'art--badge_default'
+  //   }
+  // },
+  // {
+  //   href: '/page1',
+  //   title: 'Dropdown Page',
+  //   icon: 'fa fa-list-ul',
+  //   child: [
+  //     {
+  //       href: '/page1/sub-page-1',
+  //       title: 'Sub Page 01',
+  //       icon: 'fa fa-file-alt'
+  //     },
+  //     {
+  //       href: '/page1/sub-page-2',
+  //       title: 'Sub Page 02',
+  //       icon: 'fa fa-file-alt'
+  //     }
+  //   ]
+  // },
+  // {
+  //   href: '/page2',
+  //   title: 'Multiple Level',
+  //   icon: 'fa fa-list-alt',
+  //   child: [
+  //     {
+  //       href: '/page2/sub-page-1',
+  //       title: 'page'
+  //     },
+  //     {
+  //       title: 'Level 2 ',
+  //       child: [
+  //         {
+  //           href: '/page2/sub-page-1/sub-sub-page-1',
+  //           title: 'page'
+  //         },
+  //         {
+  //           href: '/page2/sub-page-1/sub-sub-page-2',
+  //           title: 'Page'
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       href: '/page2/sub-page-2',
+  //       title: 'Page'
+  //     },
+  //     {
+  //       title: 'Another Level 2',
+  //       child: [
+  //         {
+  //           title: 'Level 3',
+  //           child: [
+  //             {
+  //               title: 'Page'
+  //             },
+  //             {
+  //               title: 'Page'
+  //             }
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 ]
