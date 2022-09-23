d5f2b98a9d36ee99b638a8bfc5673ab0bf3f34dc
teddy <hohoworld11@gmail.com>
2022-09-23 10:16:11 +0900

    HomeOrMain route depending on isAuthenticated

---
+++ src/components/menu/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    메인 화면 입니다(비로그인)
+    로그인 후엔 home화면이 보여집니다.
+  </div>
+</template>
+
+<script>
+import { headerActivate } from '../../mixin'
+
+export default {
+  name: 'Main',
+  mixins: [headerActivate],
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
