9b0676b719669232516346c22f51501982d8f2ec
teddy <hohoworld11@gmail.com>
2022-09-25 09:03:58 +0900

    profile tab ing

--- src/components/tabs/ProfileTab.vue
+++ src/components/tabs/ProfileTab.vue
@@ -4,14 +4,13 @@
       <profile-tab-item
         v-for="item in list"
         v-bind="item" :key="item.id"
-        v-model="currentId"/>
+        v-model="currentId"
+        @tabClick="tabClick"/>
     </div>
     <div class="contents">
-      <transition>
-        <section class="item" :key="currentId">
-          {{ current.content }}
-        </section>
-      </transition>
+      <section class="item" :key="currentId">
+        {{ current.content }}
+      </section>
     </div>
   </div>
 </template>
@@ -38,33 +37,14 @@ export default {
     current() {
       return this.list.find(el => el.id === this.currentId) || {}
     }
+  },
+  methods: {
+    tabClick(id) {
+      this.currentId = id
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.contents {
-  position: relative;
-  overflow: hidden;
-  width: 280px;
-  border: 2px solid #000;
-
-  .item {
-    box-sizing: border-box;
-    padding: 10px;
-    width: 100%;
-    transition: all 0.8s ease;
-  }
-}
-
-/* 트랜지션 전용 스타일 */
-.v-leave-active {
-  position: absolute;
-}
-.v-enter {
-  transform: translateX(-100%);
-}
-.v-leave-to {
-  transform: translateX(100%);
-}
 </style>
