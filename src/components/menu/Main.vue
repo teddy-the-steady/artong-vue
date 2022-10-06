49e6226451a68486eb942e9c9494aa4896b88988
mosquito1717 <ymj08073@gmail.com>
2022-10-06 10:21:56 +0900

    너비 840px까지 대응 가능

--- src/components/menu/Main.vue
+++ src/components/menu/Main.vue
@@ -1,7 +1,130 @@
 <template>
-  <div>
-    메인 화면 입니다(비로그인)
-    로그인 후엔 home화면이 보여집니다.
+  <div class="info">
+    <div class="container1">
+      <div class="intro-title">
+        Open
+        Art NFT
+        Marketplace
+      </div>
+      <div class="intro-content">
+        Lorem ipsum dolor sit amet, consectetur Arcu fames nisi malesuada vitae accumsan egestas. Quis sed pharetra lacus sed malesuada dui augue egestas. Arcu nisl egestas massa morbi fringilla facilisis eget. Eleifend tempus in ultricies suspendisse egestas.Lorem ipsum dolor sit amet,     
+      </div>
+      <button class="start-btn">Start contributing</button>
+    </div>
+    <div class="container2">
+      <div class="title">
+        Show your Creativity
+      </div>
+      <!-- Wallet connection -->
+      <svg class="intro-img" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_114_10910)">
+        <path d="M8.75293 42.8689V19.4161C5.14409 18.4871 2.43812 15.314 2.20912 11.4727L2.19141 11.4912V42.8689C2.19141 48.9133 7.09133 53.8131 13.1356 53.8131H19.6971C13.6529 53.8131 8.75293 48.9133 8.75293 42.8689Z" fill="#CCCCCC"/>
+        <path d="M53.8086 15.4279C55.0166 15.4279 55.9958 14.4487 55.9958 13.2407V10.9442C55.9958 4.90955 51.0861 0 45.0515 0C45.0515 0 10.7666 0.00513986 10.6821 0.0150915C7.7962 0.11592 5.02407 1.3543 3.02586 3.44808C0.977684 5.59414 -0.0900953 8.38541 0.0104054 11.3222C0.00624979 11.3779 0.00406261 42.8686 0.00406261 42.8686C0.00406261 50.1093 5.89478 56 13.1354 56H45.0516C51.0862 56 55.9958 51.0905 55.9958 45.0558V28.4499C55.9958 22.4153 51.0862 17.5057 45.0516 17.5057H10.9398C7.42821 17.5057 4.54923 14.761 4.38552 11.2572C4.3023 9.4755 4.94325 7.77475 6.19016 6.46813C7.45511 5.14271 9.23165 4.38266 11.0641 4.38266C11.1287 4.38266 45.0513 4.37435 45.0513 4.37435C48.674 4.37435 51.6212 7.32157 51.6212 10.9442V13.2407C51.6214 14.4487 52.6006 15.4279 53.8086 15.4279ZM10.9398 21.8801H45.0515C48.6741 21.8801 51.6213 24.8273 51.6213 28.4499V45.0558C51.6213 48.6784 48.6741 51.6257 45.0515 51.6257H13.1354C8.3068 51.6257 4.37841 47.6973 4.37841 42.8686V19.7017C6.20777 21.072 8.48079 21.8801 10.9398 21.8801Z" fill="black"/>
+        <path d="M47.2462 36.7537C47.2462 38.2636 46.0222 39.4876 44.5123 39.4876C43.0024 39.4876 41.7783 38.2636 41.7783 36.7537C41.7783 35.2437 43.0024 34.0197 44.5123 34.0197C46.0222 34.0197 47.2462 35.2437 47.2462 36.7537ZM47.2462 10.945C47.2462 9.73701 46.267 8.75781 45.0591 8.75781H10.9391C9.73115 8.75781 8.75195 9.73701 8.75195 10.945C8.75195 12.153 9.73115 13.1322 10.9391 13.1322H45.0591C46.2669 13.1322 47.2462 12.153 47.2462 10.945Z" fill="#F22E3E"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_114_10910">
+        <rect width="56" height="56" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+      <div class="subtitle">
+        Wallet connection
+      </div>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu fames nisi malesuada vitae accumsan egestas. Quis sed pharetra lacus sed malesuada dui augue egestas. Arcu nisl egestas massa morbi fringilla facilisis eget. Eleifend tempus in ultricies suspendisse egestas.
+      </div>
+      <!-- Create your collection -->
+      <svg class="intro-img" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_114_12730)">
+        <path d="M19.8803 53.8125H13.125C7.08444 53.8125 2.1875 48.9156 2.1875 42.875V13.125C2.1875 7.08433 7.08444 2.1875 13.125 2.1875H19.8803C13.8397 2.1875 8.94283 7.08433 8.94283 13.125V42.875C8.94283 48.9156 13.8397 53.8125 19.8803 53.8125Z" fill="#CCCCCC"/>
+        <path d="M42.875 56H13.125C5.88788 56 0 50.1121 0 42.875V13.125C0 5.88788 5.88788 0 13.125 0H22.2031C25.9626 0 29.5442 1.5785 32.0297 4.3307C32.8394 5.22736 32.769 6.61063 31.8724 7.42033C30.9759 8.23003 29.5925 8.15959 28.7828 7.26305C27.1252 5.42762 24.7271 4.375 22.2031 4.375H13.125C8.30025 4.375 4.375 8.30025 4.375 13.125V42.875C4.375 47.6997 8.30025 51.625 13.125 51.625H42.875C47.6997 51.625 51.625 47.6997 51.625 42.875V22.0938C51.625 18.4752 48.6811 15.5312 45.0625 15.5312H19.9062C16.2877 15.5312 13.3438 18.4752 13.3438 22.0938V44.0781C13.2362 46.9774 9.07528 46.9753 8.96875 44.0781V22.0938C8.96875 16.0628 13.8753 11.1562 19.9062 11.1562H45.0625C51.0934 11.1562 56 16.0628 56 22.0938V42.875C56 50.1121 50.1121 56 42.875 56Z" fill="black"/>
+        <path d="M32.3748 42.1112C31.1667 42.1112 30.1873 41.1318 30.1873 39.9237V35.2205H25.4842C22.5849 35.113 22.5871 30.9521 25.4842 30.8455H30.1873V26.1424C30.2949 23.2431 34.4558 23.2453 34.5623 26.1424V30.8455H39.2655C42.1648 30.9531 42.1626 35.114 39.2655 35.2205H34.5623V39.9237C34.5623 41.1318 33.583 42.1112 32.3748 42.1112Z" fill="#F22E3E"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_114_12730">
+        <rect width="56" height="56" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+      <div class="subtitle">
+        Create your project
+      </div>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu fames nisi malesuada vitae accumsan egestas. Quis sed pharetra lacus sed malesuada dui augue egestas. Arcu nisl egestas massa morbi fringilla facilisis eget. Eleifend tempus in ultricies suspendisse egestas.
+      </div>
+      <!-- Add you NFTs -->
+      <svg class="intro-img" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_116_14662)">
+        <path d="M8.77016 28.0177C8.76994 14.8878 18.5817 4.04953 31.2731 2.43528C16.0196 0.389389 2.09691 12.6077 2.21362 28.0178C2.10609 43.3895 16.0656 55.6555 31.289 53.5979C18.6315 51.9805 8.77038 41.1135 8.77016 28.0177Z" fill="#CCCCCC"/>
+        <path d="M25.6638 32.7084C24.4197 32.7237 23.333 31.6187 23.3691 30.375C23.3743 29.757 23.6197 29.1778 24.0662 28.7387L32.7094 20.1756C33.5669 19.3261 33.5733 17.9422 32.7239 17.0848C31.8743 16.2274 30.4906 16.2209 29.6331 17.0704L20.9957 25.6279C19.7226 26.88 19.013 28.553 18.9983 30.3385C18.9833 32.1241 19.6647 33.8088 20.9168 35.0818C22.2233 36.4104 23.953 37.0765 25.6838 37.0765C27.3769 37.0764 29.071 36.439 30.3708 35.1609C30.381 35.1509 38.8533 26.4894 38.8533 26.4894C39.6974 25.6265 39.682 24.2426 38.8193 23.3987C37.9564 22.5548 36.5727 22.5699 35.7286 23.4328L27.2933 32.0567C26.8539 32.4822 26.2764 32.7094 25.6638 32.7084Z" fill="black"/>
+        <path d="M28.1509 55.9922C28.1016 55.9969 28.2004 55.9922 28.1509 55.9922V55.9922Z" fill="black"/>
+        <path d="M55.7032 24.1075C55.5344 22.9122 54.4279 22.0804 53.2335 22.2489C52.0382 22.4177 51.2062 23.5234 51.3749 24.7185C51.5341 25.8461 51.6118 26.9988 51.6058 28.1447C51.5996 29.3445 51.5023 30.5492 51.3167 31.7255C51.2076 32.4171 50.819 32.9957 50.2229 33.3543C49.6159 33.7196 48.8764 33.7946 48.1939 33.56C46.8604 33.1017 45.4634 32.8951 44.0397 32.9438C38.0177 33.1532 33.1364 38.0344 32.9268 44.0566C32.8774 45.4796 33.0846 46.8773 33.543 48.2109C33.7775 48.8933 33.7026 49.6329 33.3373 50.2398C32.9787 50.836 32.4002 51.2246 31.7087 51.3336C30.5322 51.5193 29.3273 51.6166 28.1275 51.6228C15.4545 51.9006 4.11776 40.7257 4.39871 28.0194C4.13601 15.3322 15.3154 4.1521 28.0032 4.41524C29.3105 4.41524 30.6226 4.52408 31.9029 4.73881C33.0934 4.93802 34.2202 4.13549 34.4197 2.9448C34.6194 1.75433 33.8162 0.627569 32.6257 0.42792C15.9043 -2.61852 -0.226892 11.0197 0.0275063 28.0198C-0.30426 43.086 13.1216 56.3248 28.1503 55.9939C29.5699 55.9864 30.9964 55.8711 32.3901 55.6513C36.4533 55.0915 39.0859 50.6324 37.6769 46.7902C36.0749 42.3828 39.491 37.3906 44.1917 37.3122C45.0778 37.2812 45.947 37.4097 46.7734 37.6936C48.6807 38.3488 50.7595 38.1327 52.4764 37.0996C54.1738 36.0783 55.3248 34.3679 55.6344 32.4068C55.8543 31.0133 55.9695 29.587 55.977 28.1675C55.984 26.8103 55.892 25.4444 55.7032 24.1075Z" fill="black"/>
+        <path d="M44.1595 21.1329C39.6739 21.2069 35.5993 17.5792 35.2552 13.1031C35.0298 10.7438 35.7428 8.43857 37.2619 6.61255C39.3296 4.12747 41.8154 3.28734 44.2195 2.47508C45.6523 1.99087 47.1339 1.49016 48.7168 0.613103C50.1507 -0.181235 51.7993 -0.149872 53.2401 0.698995C54.9031 1.67889 55.9778 3.58993 55.9778 5.56785C55.9778 8.47507 54.9366 12.1791 53.3253 15.0045C51.0714 18.9564 47.8163 21.1329 44.1595 21.1329ZM50.8967 4.41508C50.8742 4.41508 50.8576 4.42426 50.8353 4.4365C48.907 5.50491 47.1601 6.09523 45.6189 6.61604C43.4596 7.34569 41.8995 7.87295 40.622 9.4083C39.8535 10.332 39.4927 11.4967 39.6064 12.6876C39.7205 13.8822 40.2978 14.9617 41.2321 15.7272C44.5609 18.2636 47.9649 15.7878 49.5281 12.8391C50.964 10.3214 51.6067 7.3729 51.6067 5.56774C51.6067 5.07556 51.305 4.63211 51.0213 4.46491C50.9586 4.4282 50.9235 4.41508 50.8967 4.41508ZM27.5656 8.78454C26.0569 8.78454 24.8337 10.0077 24.8337 11.5165C24.9677 15.1364 30.1642 15.1354 30.2976 11.5165C30.2976 10.0077 29.0744 8.78454 27.5656 8.78454ZM17.7307 13.0464C16.2219 13.0464 14.9987 14.2695 14.9987 15.7783C15.1327 19.3982 20.3292 19.3972 20.4626 15.7783C20.4626 14.2695 19.2395 13.0464 17.7307 13.0464ZM11.939 21.6793C10.4302 21.6793 9.20703 22.9024 9.20703 24.4112C9.34101 28.0311 14.5375 28.0302 14.6709 24.4112C14.6709 22.9024 13.4478 21.6793 11.939 21.6793ZM10.7369 34.6833C10.7369 36.1921 11.9601 37.4152 13.4689 37.4152C17.0888 37.2813 17.0878 32.0848 13.4689 31.9514C11.9601 31.9514 10.7369 33.1745 10.7369 34.6833Z" fill="#F22E3E"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_116_14662">
+        <rect width="56" height="56" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+      <div class="subtitle">
+        Add you NFTs
+      </div>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu fames nisi malesuada vitae accumsan egestas. Quis sed pharetra lacus sed malesuada dui augue egestas. Arcu nisl egestas massa morbi fringilla facilisis eget. Eleifend tempus in ultricies suspendisse egestas.
+      </div>
+      <!-- Share the collection -->
+      <svg class="intro-img" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21.4375 15.3125C21.4375 9.21463 25.6363 4.12081 31.2812 2.65344C30.2262 2.37923 29.1397 2.1875 28 2.1875C20.7624 2.1875 14.875 8.07494 14.875 15.3125C14.875 22.5501 20.7624 28.4375 28 28.4375C29.1397 28.4375 30.2262 28.2458 31.2812 27.9716C25.6363 26.5042 21.4375 21.4104 21.4375 15.3125Z" fill="#CCCCCC"/>
+        <path d="M12.6875 43.2513V40.6875C12.6875 39.4813 13.6688 38.5 14.875 38.5H19.25C20.4582 38.5 21.4375 37.5207 21.4375 36.3125C21.4375 35.1043 20.4582 34.125 19.25 34.125H14.875C11.2564 34.125 8.3125 37.0689 8.3125 40.6875V43.2513C5.76647 44.1543 3.9375 46.5858 3.9375 49.4375C3.9375 53.0561 6.88144 56 10.5 56C14.1186 56 17.0625 53.0561 17.0625 49.4375C17.0625 46.5858 15.2335 44.1544 12.6875 43.2513ZM10.5 51.625C9.29381 51.625 8.3125 50.6437 8.3125 49.4375C8.3125 48.2313 9.29381 47.25 10.5 47.25C11.7062 47.25 12.6875 48.2313 12.6875 49.4375C12.6875 50.6437 11.7062 51.625 10.5 51.625Z" fill="#F22E3E"/>
+        <path d="M41.125 34.125H36.75C35.5418 34.125 34.5625 35.1043 34.5625 36.3125C34.5625 37.5207 35.5418 38.5 36.75 38.5H41.125C42.3312 38.5 43.3125 39.4813 43.3125 40.6875V43.2513C40.7665 44.1543 38.9375 46.5858 38.9375 49.4375C38.9375 53.0561 41.8814 56 45.5 56C49.1186 56 52.0625 53.0561 52.0625 49.4375C52.0625 46.5858 50.2335 44.1544 47.6875 43.2513V40.6875C47.6875 37.0689 44.7436 34.125 41.125 34.125ZM45.5 51.625C44.2938 51.625 43.3125 50.6437 43.3125 49.4375C43.3125 48.2313 44.2938 47.25 45.5 47.25C46.7062 47.25 47.6875 48.2313 47.6875 49.4375C47.6875 50.6437 46.7062 51.625 45.5 51.625Z" fill="#F22E3E"/>
+        <path d="M28 21.875C31.6186 21.875 34.5625 18.9311 34.5625 15.3125C34.5625 11.6939 31.6186 8.75 28 8.75C24.3814 8.75 21.4375 11.6939 21.4375 15.3125C21.4375 18.9311 24.3814 21.875 28 21.875ZM28 13.125C29.2062 13.125 30.1875 14.1063 30.1875 15.3125C30.1875 16.5187 29.2062 17.5 28 17.5C26.7938 17.5 25.8125 16.5187 25.8125 15.3125C25.8125 14.1063 26.7938 13.125 28 13.125Z" fill="black"/>
+        <path d="M25.8125 30.468V43.2513C23.2665 44.1543 21.4375 46.5858 21.4375 49.4375C21.4375 53.0561 24.3814 56 28 56C31.6186 56 34.5625 53.0561 34.5625 49.4375C34.5625 46.5858 32.7335 44.1544 30.1875 43.2513V30.468C37.5982 29.4033 43.3125 23.0132 43.3125 15.3125C43.3125 6.86919 36.4433 0 28 0C19.5567 0 12.6875 6.86919 12.6875 15.3125C12.6875 23.0132 18.4018 29.4034 25.8125 30.468ZM28 51.625C26.7938 51.625 25.8125 50.6437 25.8125 49.4375C25.8125 48.2313 26.7938 47.25 28 47.25C29.2062 47.25 30.1875 48.2313 30.1875 49.4375C30.1875 50.6437 29.2062 51.625 28 51.625ZM28 4.375C34.031 4.375 38.9375 9.28156 38.9375 15.3125C38.9375 21.3434 34.031 26.25 28 26.25C21.969 26.25 17.0625 21.3434 17.0625 15.3125C17.0625 9.28156 21.969 4.375 28 4.375Z" fill="black"/>
+      </svg>
+      <div class="subtitle">
+        Share the collection
+      </div>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu fames nisi malesuada vitae accumsan egestas. Quis sed pharetra lacus sed malesuada dui augue egestas. Arcu nisl egestas massa morbi fringilla facilisis eget. Eleifend tempus in ultricies suspendisse egestas.
+      </div>
+    </div> <!--this is the end of container2-->
+    <div class="container3">
+      <div class="button-block">
+        <svg class="button-block-img" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11 9.5999V12.5999H7.99999V14.5999H11V17.5999H13V14.5999H16V12.5999H13V9.5999H11ZM11.2929 6.30701L11.5858 6.5999H12H21.6C21.9314 6.5999 22.2 6.86853 22.2 7.1999V19.9999C22.2 20.3313 21.9314 20.5999 21.6 20.5999H2.39999C2.06862 20.5999 1.79999 20.3313 1.79999 19.9999V3.9999C1.79999 3.66853 2.06862 3.3999 2.39999 3.3999H8.38578L11.2929 6.30701Z" fill="#FEEAEC" stroke="#F22E3E" stroke-width="2"/>
+        </svg>
+
+        <div class="button-block-title">
+          Create new collection
+        </div>
+        <div class="button-block-description">
+          Create Your Own NFT Collection
+        </div>
+      </div>
+      <div class="button-block">
+        <svg class="button-block-img" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11 6.39981V10.9998H6.39999V12.9998H11V17.5998H13V12.9998H17.6V10.9998H13V6.39981H11ZM12 22.1998C6.36668 22.1998 1.79999 17.6331 1.79999 11.9998C1.79999 6.3665 6.36668 1.7998 12 1.7998C17.6333 1.7998 22.2 6.3665 22.2 11.9998C22.2 17.6331 17.6333 22.1998 12 22.1998Z" fill="#FEEAEC" stroke="#F22E3E" stroke-width="2"/>
+        </svg>
+
+        <div class="button-block-title">
+          Create new collection
+        </div>
+        <div class="button-block-description">
+          Create Your Own NFT Collection
+        </div>
+      </div>
+    </div> <!--end of container3-->
+    <div class="container4">
+      <div class="top-container">      
+        <div class="curated-collections">Curated Collections</div>
+        <div class="url" href="">View more</div>
+      </div>
+      <div class="collection-card">
+        
+      </div>
+    </div> <!--end of container4-->
   </div>
 </template>
 
@@ -15,4 +138,224 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @media (max-width:1080px){
+  .container1{
+    display: flex;
+    flex-direction: column;
+
+    margin-left:16px;
+    margin-right:16px;
+    margin-top: 48px;
+    row-gap: 48px;
+  }
+  .intro-title{
+    width: 295px;
+    height: 216px;
+
+    font-family: 'Mustica Pro';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 52px;
+    line-height: 72px;
+    text-transform: capitalize;
+
+    color: #000000;
+
+    text-align:left;    
+  }
+  .intro-content{
+    width: 91.1%;
+    //height: 133px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+
+    color: #4D4D4D;
+    align-self:stretch;
+
+    text-align:left;
+  }
+  .start-btn{
+    padding: 15px 24px 16px;
+    width: 161px;
+    height: 48px;
+
+    background: #000000;
+    border-radius: 5px;
+    text-transform: none;
+    margin-bottom:48px;
+  }
+  .container2{
+    display: flex;
+    flex-direction: column;
+    margin-top: 48px;
+  }
+
+  .title{
+    width: 308px;
+    height: 44px;
+
+    font-family: 'Mustica Pro';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 44px;
+    margin: auto;
+    margin-bottom: 48px;
+
+    color: #000000;
+  }
+  .intro-img{
+    width: 32px;
+    height: 32px;
+    margin: auto;
+    margin-bottom:16px;
+  }
+  .subtitle{
+    width: 185px;
+    height: 24px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 24px;
+
+    color: #000000;
+
+    margin: auto;
+    margin-bottom: 16px;
+  }
+  .content{
+    width: 91.1%;
+    max-width: 432px;
+    //height: 168px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    
+    text-align: center;
+
+    color: #4D4D4D;
+
+    margin:auto;
+    margin-bottom:40px;
+  }
+  .container3{
+    display: flex;
+    flex-direction:column;
+    row-gap: 40px;
+    margin-top: 48px;
+    margin-bottom: 48px;
+  }
+  .button-block{
+    width: 91.1%;
+    height: 160px;
+
+    background: #FFFFFF;
+    border: 1px solid #E5E5E5;
+    box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.14);
+    border-radius: 14px;
+
+    margin:auto;
+
+    // 격자
+    background-image: linear-gradient(90deg, rgba(242,46,62,0.1) 1px, transparent 1px), linear-gradient(0deg, rgba(242,46,62,0.1) 1px, transparent 1px);
+    background-size: 32px 32px;
+  }
+
+  .button-block-img{
+    width: 24px;
+    height: 24px;
+
+    margin: auto;
+    margin-top: 25px;
+  }
+  .button-block-title{
+    width: 181px;
+    height: 22px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 22px;
+
+    color: #000000;
+
+    margin:auto;
+    margin-top: 10.4px;
+    margin-bottom: 8px;
+    
+  }
+  .button-block-description{
+    width: 248px;
+    height: 48px; 
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+
+    color: #4D4D4D;
+
+    margin:auto;
+    margin-bottom: 25px;
+  }
+  }
+  @media(min-width: 1080px){
+
+  }
+  // curated collections
+  .container4{
+    //width: 360px;
+    height: 983px;
+
+    background: #000000;
+
+    padding-top: 48px;
+  }
+  .top-container{
+    display:flex;
+    flex-direction: row;
+  }
+  .curated-collections{
+    width: 200px;
+    height: 30px;
+
+    font-family: 'Mustica Pro';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 30px;
+
+    color: #B3B3B3;
+    
+    margin-left: 24px;
+  }
+  .url{
+    width: 58px;
+    height: 14px;
+
+    margin-left: 54px;
+    margin-top: 16px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    text-decoration-line: underline;
+    color: #F22E3E;
+  }
+  .collection-card{
+    
+  }
 </style>
