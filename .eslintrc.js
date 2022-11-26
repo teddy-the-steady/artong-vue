1dfcda523a411d251ac761d61d98c2be7b966ec3
teddy <teddy.jeon@speedwagon.io>
2022-11-26 14:56:49 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+  extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "semi": ["error", "never"],
+    "space-before-blocks": ["error", "always"],
+    "keyword-spacing": ["error", { "before": true }],
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "prettier/prettier": [
+			"error",
+			{
+				singleQuote: true,
+				semi: false,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+			},
+		],
   },
 };
