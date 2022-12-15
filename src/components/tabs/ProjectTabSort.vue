d7cf6b84432942f963dd0b4cebfef9f3eb370e6d
mosquito1717 <ymj08073@gmail.com>
2022-12-15 14:41:47 +0900

    completed project's tab

--- src/components/tabs/ProjectTabSort.vue
+++ src/components/tabs/ProjectTabSort.vue
@@ -8,6 +8,10 @@
         v-model="currentId"
         @tabClick="tabClick"
       />
+      <div class="newest">
+        <div>Newest</div>
+        <img src="../../assets/icons/arrow_down.svg" />
+      </div>
     </div>
     <div class="items">
       <section class="item" :key="currentId">
@@ -73,5 +77,33 @@ export default {
   border-bottom: 1px solid #b3b3b3;
   margin: 0px 24px 0px 24px;
   display: flex;
+  .newest {
+    margin-left: auto;
+    display: flex;
+    justify-content: space-between;
+    vertical-align: center;
+    padding-left: 16px;
+    padding-right: 14px;
+    box-sizing: border-box;
+
+    width: 105px;
+    height: 36px;
+    background: #ffffff;
+    border: 1px solid #f2f2f2;
+    box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.08);
+    border-radius: 999px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    color: #000000;
+    line-height: 36px;
+    img {
+      width: 9.5px;
+      height: 5.9px;
+      margin-top: 14px;
+    }
+  }
 }
 </style>
