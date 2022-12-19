6b3ea792a43c8f502f9a7a5283fccadf859eca1e
dhkim <seumconsult@gmail.com>
2022-12-19 16:45:46 +0900

    토큰 상세 1차 적용. ContentCard.vue : Prop 적용 ContentsProfile.vue : username 표시 재 활성 TokensByCollection.vue : More from this collection 용 컴포넌트 추가

---
+++ src/components/CollectionCard/TokensByCollection.vue
@@ -0,0 +1,44 @@
+<template>
+  <carousel
+    :perPageCustom="[
+      [360, 1],
+      [400, 1.2],
+      [500, 1.5],
+      [600, 1.8],
+      [700, 2],
+      [840, 2.2],
+      [1080, 2.9],
+      [1440, 3.9],
+      [1920, 4],
+    ]"
+    :autoplay="true"
+    :autoplayHoverPause="true"
+    :loop="true"
+    :scrollPerPage="true"
+    paginationColor="#B3B3B3"
+    paginationActiveColor="#000000"
+  >
+    <slide v-for="(content, i) in tokens" :key="i">
+      <ContentCard :content="content" :needContentName="false"></ContentCard>
+    </slide>
+  </carousel>
+</template>
+<script>
+import ContentCard from '../contents_v3/ContentCard.vue'
+
+export default {
+  name: 'TokensByCollection',
+  components: {
+    ContentCard,
+  },
+  props: {
+    tokens: {
+      type: Array,
+      default: null,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
