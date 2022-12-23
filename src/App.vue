a0dbcad3fdf8c7292968de1dc9989fdff74d50ce
teddy <hohoworld11@gmail.com>
2022-12-23 11:33:11 +0900

    ios appstore redirect debug

--- src/App.vue
+++ src/App.vue
@@ -111,10 +111,7 @@ export default {
       if (member) {
         await this.$store.dispatch('CURRENT_USER', member)
         if (this.isMobile) {
-          await this.$store.dispatch(
-            'AUTO_CONNECT_WALLET',
-            this.getDefaultWalletConnectState,
-          )
+          await this.$store.dispatch('AUTO_CONNECT_WALLET')
         } else {
           this.addPcWalletEventHandler()
           await this.getPcWalletOnFirstLoad()
