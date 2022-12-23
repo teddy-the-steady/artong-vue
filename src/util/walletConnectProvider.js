376a2613ed5dd6d03a19b71f1715f3ed26c06c4c
teddy <hohoworld11@gmail.com>
2022-12-23 13:56:26 +0900

    getSigner into Provider class

--- src/util/walletConnectProvider.js
+++ src/util/walletConnectProvider.js
@@ -1,6 +1,10 @@
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import { ethers } from 'ethers'
 
 class Provider {
+  pcProvider
+  mobileProvider
+
   constructor() {
     this.provider = new WalletConnectProvider({
       infuraId: process.env.VUE_APP_INFURA_ID,
@@ -17,6 +21,19 @@ class Provider {
         mobileLinks: ['metamask', 'rainbow'],
       },
     })
+  }
+
+  async getPcSigner() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+    this.pcProvider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = this.pcProvider.getSigner()
+    return signer
+  }
+
+  getWalletConnectSigner() {
+    this.mobileProvider = new ethers.providers.Web3Provider(Provider.provider)
+    const signer = this.mobileProvider.getSigner()
+    return signer
   }
 }
 
