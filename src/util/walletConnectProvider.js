41138c7902a5b1cf5fb63dea2cfa9d62b2f79e35
taeho_jeon <superduper8989@gmail.com>
2022-12-26 14:08:21 +0900

    isSessionValid, checkMobileWalletStatusAndGetSigner added

--- src/util/walletConnectProvider.js
+++ src/util/walletConnectProvider.js
@@ -2,9 +2,6 @@ import WalletConnectProvider from '@walletconnect/web3-provider'
 import { ethers } from 'ethers'
 
 class Provider {
-  pcProvider
-  mobileProvider
-
   constructor() {
     this.provider = new WalletConnectProvider({
       infuraId: process.env.VUE_APP_INFURA_ID,
@@ -24,16 +21,24 @@ class Provider {
   }
 
   async getPcSigner() {
+    if (this.pcSigner) {
+      return this.pcSigner
+    }
+
     await window.ethereum.request({ method: 'eth_requestAccounts' })
     this.pcProvider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = this.pcProvider.getSigner()
-    return signer
+    this.pcSigner = this.pcProvider.getSigner()
+    return this.pcSigner
   }
 
   getWalletConnectSigner() {
+    if (this.mobileSigner) {
+      return this.mobileSigner
+    }
+
     this.mobileProvider = new ethers.providers.Web3Provider(this.provider)
-    const signer = this.mobileProvider.getSigner()
-    return signer
+    this.mobileSigner = this.mobileProvider.getSigner()
+    return this.mobileSigner
   }
 }
 
