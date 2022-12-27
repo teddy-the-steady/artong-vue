48599d4ce6cb0a4b237af4203ed8990006231015
mosquito1717 <ymj08073@gmail.com>
2022-12-27 15:05:02 +0900

    project info 버튼 클릭시 나타나는 모달창 완성

---
+++ src/components/projects/ProjectInfo.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="wrapper">
+    <div class="top">
+      <div class="img">
+        <svg
+          class="logo"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1690 300"
+        >
+          <g>
+            <path
+              d="M1614.9,90c41.48,0,75.1,33.62,75.1,75.1v-35.11c0-71.79-58.2-129.99-129.99-129.99h0c-6.8,0-13.49,.52-20.01,1.53-62.3,9.62-110,63.48-110,128.48v40c0,16.18,2.96,31.67,8.37,45.95,13.18,34.87,40.92,62.58,75.81,75.72,8.24,3.1,16.88,5.4,25.82,6.78,6.52,1.01,13.19,1.53,19.99,1.53h0c16.18,0,31.67-2.97,45.97-8.37,34.85-13.17,62.54-40.89,75.7-75.74,5.01-13.29,7.92-27.61,8.3-42.56-.89-29.6-25.14-53.33-54.96-53.33s-55,24.62-55,55v15c0,11.05-8.95,20-20,20s-20-8.95-20-20v-25.1c0-41.37,33.53-74.9,74.9-74.9Z"
+            />
+            <path
+              d="M805,0h-189.84c-30.46,0-55.16,24.69-55.16,55.16h0c0,30.29,24.55,54.84,54.84,54.84h.34c1.77-26.64,17.43-49.47,39.81-61.32,10.46-5.54,22.38-8.68,35.04-8.68h0c39.73,0,72.22,30.92,74.78,70h-.06c-2.53-28.03-26.07-50-54.76-50h0c-28.69,0-52.24,21.97-54.76,50h-.24V244.84c0,30.46,24.69,55.16,55.16,55.16h0c30.29,0,54.84-24.55,54.84-54.84V110h40c30.38,0,55-24.62,55-55h0c0-30.38-24.62-55-55-55Z"
+            />
+            <path
+              d="M130.01,0h0c-6.8,0-13.49,.52-20.01,1.53C47.7,11.16,0,65.01,0,130.01v114.83c0,30.46,24.69,55.16,55.16,55.16h0c30.29,0,54.84-24.55,54.84-54.84v-80.26c0-41.37,33.53-74.9,74.9-74.9h0c41.48,0,75.1,33.62,75.1,75.1v-35.11C260,58.2,201.8,0,130.01,0Z"
+            />
+            <path
+              d="M205,120h0c-30.38,0-55,24.62-55,55v69.84c0,30.46,24.69,55.16,55.16,55.16h0c30.29,0,54.84-24.55,54.84-54.84v-70.16c0-30.38-24.62-55-55-55Z"
+            />
+            <path
+              d="M1355,0h-75c-6.8,0-13.48,.53-20,1.53-62.3,9.62-110,63.48-110,128.48v114.83c0,30.46,24.69,55.16,55.16,55.16h0c30.29,0,54.84-24.55,54.84-54.84v-100.26c0-19.67,7.6-37.56,20-50.93,5.73-6.17,12.49-11.38,20-15.34,.13-.07,.26-.13,.4-.2,10.33-5.37,22.06-8.43,34.5-8.43h0c1.72,0,3.41,.08,5.1,.19,1.92,.13,3.82,.32,5.7,.59-25.93,4.38-45.68,26.92-45.7,54.1v.04h0v120c0,13.33,4.74,25.55,12.61,35.08,10.1,12.22,25.37,20,42.46,20h.16c17.05,0,32.27-7.79,42.31-20,7.78-9.46,12.45-21.56,12.45-34.76V55c0-30.38-24.62-55-55-55Z"
+            />
+            <path
+              d="M1000,0h0c-71.8,0-130,58.2-130,130v40c0,71.8,58.2,130,130,130h0c71.8,0,130-58.2,130-130v-40c0-71.8-58.2-130-130-130Zm55,158.46c0,30.38-24.62,55-55,55h0c-30.38,0-55-24.62-55-55v-16.92c0-30.38,24.62-55,55-55h0c30.38,0,55,24.62,55,55v16.92Z"
+            />
+            <path
+              d="M485,0h-150c-30.38,0-55,24.62-55,55V244.84c0,30.46,24.69,55.16,55.16,55.16h0c30.29,0,54.84-24.55,54.84-54.84v-100.26c0-19.67,7.6-37.56,20-50.93,5.73-6.17,12.49-11.38,20-15.34,10.42-5.5,22.29-8.63,34.9-8.63h0c1.72,0,3.41,.08,5.1,.19,1.92,.13,3.82,.32,5.7,.59-25.93,4.38-45.68,26.92-45.7,54.1v.04c0,30.42,24.66,55.08,55.08,55.08h.16c27.13,0,49.64-19.73,53.99-45.62,.41-2.47,.66-4.99,.73-7.56,.03,1.05,.04,2.11,.04,3.17V55c0-30.38-24.62-55-55-55Z"
+            />
+            <path
+              d="M485.08,190h-.16c-17.94,0-33.86,8.6-43.89,21.9-6.93,9.19-11.04,20.62-11.04,33.02,0,13.33,4.74,25.55,12.61,35.08,10.1,12.22,25.37,20,42.46,20h.16c17.05,0,32.27-7.79,42.31-20,7.78-9.46,12.45-21.56,12.45-34.76v-.31c0-8.98-2.17-17.44-5.98-24.92-9.08-17.8-27.58-30-48.94-30Z"
+            />
+          </g>
+        </svg>
+      </div>
+      <img class="delete-btn" src="../../assets/icons/delete.svg" />
+    </div>
+    <div class="title">Project Info</div>
+    <div class="container1" v-for="(item, index) in data1" :key="index">
+      <div class="name">{{ item.name }}</div>
+      <div class="info">{{ item.info }}</div>
+    </div>
+    <div class="container2">
+      <div class="btn-container" v-for="(item, index) in data2" :key="index">
+        <button class="white-btn long-btn" @click="move">
+          {{ item.name }}
+        </button>
+      </div>
+    </div>
+    <div class="container3">
+      <button class="white-btn subscribe">Subscribe</button>
+      <button class="contribute">Contribute</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectInfo',
+  components: {},
+  data() {
+    return {
+      data1: [
+        { name: 'Items', info: '235/1000' },
+        { name: 'Subscirber', info: '2,000' },
+        { name: 'Floor price', info: '0.001 ETH' },
+        { name: 'Total sales', info: '12K ETH' },
+      ],
+      data2: [
+        { name: 'Instagram', link: 'https://www.naver.com/' },
+        { name: 'twitter', link: 'https://www.naver.com/' },
+        { name: 'discord', link: 'https://naver.com' },
+        { name: 'website', link: 'https://naver.com' },
+      ],
+    }
+  },
+  methods: {
+    move() {
+      window.open(this.data2.link)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.wrapper {
+  padding-left: 16px;
+  padding-right: 16px;
+
+  .top {
+    text-align: initial;
+    display: flex;
+    .logo {
+      width: 111.33px;
+      height: 20px;
+      margin-top: 30px;
+    }
+    .delete-btn {
+      margin-top: 16px;
+      margin-left: auto;
+      margin-bottom: 16px;
+      width: 48px;
+      height: 48px;
+    }
+  }
+  .title {
+    justify-content: center;
+    border-bottom: 1px solid #e5e5e5;
+    height: 61px;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: $artong-black;
+    line-height: 61px;
+    // 선의 너비가 화먼 너비와 같게
+    width: 100vw;
+    margin-left: -16px;
+    margin-right: -16px;
+  }
+  .container1 {
+    text-align: initial;
+    margin-top: 16px;
+    margin-bottom: 8px;
+    .name {
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      color: $artong-black;
+      margin-bottom: 8px;
+    }
+    .info {
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      color: #4d4d4d;
+    }
+  }
+  .container2 {
+    margin-top: 16px;
+    margin-bottom: 299px;
+    .btn-container {
+      margin-top: 8px;
+      .long-btn {
+        width: 100%;
+        height: 48px;
+
+        border-radius: 999px;
+      }
+    }
+  }
+  .container3 {
+    display: flex;
+    height: 72px;
+    width: 100vw;
+    margin-left: -16px;
+    margin-right: -16px;
+    border-top: 1px solid #e5e5e5;
+    justify-content: space-evenly;
+    padding-top: 24px;
+    .subscribe {
+      width: 46vw;
+      height: 48px;
+    }
+    .contribute {
+      width: 46vw;
+      height: 48px;
+    }
+  }
+}
+</style>
