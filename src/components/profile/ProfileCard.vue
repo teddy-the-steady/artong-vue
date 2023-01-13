c3b2daf1431dddf35c07c176b7f7cf94451ed895
teddy <teddy.jeon@speedwagon.io>
2023-01-13 11:06:08 +0900

    minor css mods, Project profile/background on progress

--- src/components/profile/ProfileCard.vue
+++ src/components/profile/ProfileCard.vue
@@ -36,14 +36,22 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
-.top {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .name {
-    width: 80px;
-    overflow: hidden !important;
-    text-overflow: ellipsis;
+.wrapper {
+  width: 90%;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 20px;
+  padding: 20px 0;
+  box-shadow: 2px 2px 12px rgb(0 0 0 / 14%);
+  .top {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .name {
+      width: 80px;
+      overflow: hidden !important;
+      text-overflow: ellipsis;
+    }
   }
 }
 </style>
