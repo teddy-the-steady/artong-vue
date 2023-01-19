de891f1fdd239d72c291d703af19e25e81a1625c
mosquito1717 <ymj08073@gmail.com>
2023-01-19 11:05:15 +0900

    making searchModal

--- src/components/util/Test.vue
+++ src/components/util/Test.vue
@@ -1,157 +1,60 @@
 <template>
-  <div>
+  <div class="container">
     This is a test
-    <TableDiv
-      :api="queryOffersByToken"
-      :tableName="'Offers'"
-      :iconSrc="require('@/assets/icons/100-add-folder.svg')"
-      :showHeader="true"
-      :fields="[
-        {
-          name: 'PRICE',
-          type: 'price',
-          key: 'price',
-        },
-        {
-          name: 'DATE',
-          type: 'date',
-          key: 'createdAt',
-        },
-        {
-          name: 'FROM',
-          type: 'member',
-          key: 'from',
-        },
-      ]"
-    ></TableDiv>
-    <div class="divider"></div>
-    <TableDiv
-      :api="queryTokenHistory"
-      :tableName="'History'"
-      :iconSrc="require('@/assets/icons/history.svg')"
-      :showHeader="true"
-      :fields="[
-        {
-          name: 'PRICE',
-          type: 'price',
-          key: 'price',
-        },
-        {
-          name: 'From',
-          type: 'member',
-          key: 'from_member',
-        },
-        {
-          name: 'DATE',
-          type: 'date',
-          key: 'block_timestamp',
-        },
-        {
-          name: 'TO',
-          type: 'member',
-          key: 'to_member',
-        },
-      ]"
-    ></TableDiv>
+    <button @click="openModal">click here to open modal</button>
+    <SearchModal
+      v-show="isSearchModalOpen"
+      @close-modal="toggleSearchModal"
+      ref="searchModal"
+    >
+      <span slot="body">search something</span>
+    </SearchModal>
   </div>
 </template>
 <script>
-import {
-  graphql,
-  queryOffersByToken,
-  queryTokenHistory,
-} from '../../api/graphql'
-import TableDiv from '../table/TableDiv.vue'
+import { mapState } from 'vuex'
+import SearchModal from '../../components/modal/SearchModal.vue'
 
 export default {
   name: 'Test',
   components: {
-    TableDiv,
+    SearchModal,
+  },
+  computed: {
+    ...mapState({
+      isModalOpen: state => state.menu.isModalOpen,
+      isSearchModalOpen: state => state.menu.isSearchModalOpen,
+    }),
+    isMobiel() {
+      return this.$isMobiel()
+    },
+  },
+  methods: {
+    toggleSearchModal() {
+      this.$store.commit('TOGGLE_SEARCH_MODAL')
+    },
+    setWidth() {
+      this.$store.commit('SET_INNER_WIDTH', window.innerWidth)
+    },
+    openModal() {
+      this.isSearchModalOpen = true
+    },
   },
   data() {
-    return {
-      queryOffersByToken: {
-        func: null,
-        body: {},
-      },
-      queryTokenHistory: {
-        func: null,
-        body: {},
-      },
-    }
+    return {}
   },
-  created() {
-    this.queryOffersByToken = {
-      result_key: 'offers',
-      func: graphql,
-      body: queryOffersByToken({
-        variables: {
-          first: 1,
-          skip: 0,
-          id: '0x4704cf416a4c6dcb7317cd7ac8b4b9e487159eb3' + '2',
-          //id: this.$route.params.project_address + this.$route.params.token_id,
-        },
-      }),
-    }
-    this.queryTokenHistory = {
-      result_key: 'history',
-      func: graphql,
-      body: queryTokenHistory({
-        variables: {
-          id: '0x4704cf416a4c6dcb7317cd7ac8b4b9e487159eb3' + '2',
-          //id: this.$route.params.project_address + this.$route.params.token_id,
-        },
-        pagination: {
-          start_num: 0,
-          count_num: 1,
-        },
-      }),
-    }
+  created() {},
+  watch: {
+    isModalOepn() {
+      document.body.classList.toggle('prevent-scroll')
+    },
   },
 }
 </script>
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
-* {
-  margin: 0;
-  padding: 0;
-}
-html,
-body,
-.wrapper {
-  width: 100%;
-  height: 100vh;
-}
-.wrapper .tableBox {
-  position: relative;
-  top: 50px;
-  left: 50px;
-  width: 700px;
-  height: 100px;
-  overflow-y: scroll;
-  border-top: 2px solid dodgerblue;
-}
-.tableData {
-  width: 100%;
-  border-collapse: collapse;
-  text-align: center;
-}
-.tableData thead tr th {
-  position: sticky;
-  top: 0;
-  background: #ebeaea;
-}
-.tableData thead tr th,
-.tableData tbody tr td {
-  padding: 15px 0;
-  box-sizing: border-box;
-  border-bottom: 1px solid #dedede;
-  border-right: 1px solid #ddd;
-  word-break: break-all;
-}
-.divider {
-  background-color: burlywood;
-  width: 100%;
-  height: 10px;
+
+.container {
+  padding-top: 70px;
 }
 </style>
