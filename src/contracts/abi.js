21093dad9590ef87ebc918b86f71defc41377da1
taeho_jeon <superduper8989@gmail.com>
2023-01-24 14:53:38 +0900

    abi/constants updated

--- src/contracts/abi.js
+++ src/contracts/abi.js
@@ -20,6 +20,12 @@ export const MARKETPLACE_ABI = [
         name: 'balance',
         type: 'uint256',
       },
+      {
+        indexed: false,
+        internalType: 'string',
+        name: 'reason',
+        type: 'string',
+      },
     ],
     name: 'ArtongBalanceUpdated',
     type: 'event',
@@ -489,11 +495,6 @@ export const MARKETPLACE_ABI = [
   {
     inputs: [
       {
-        internalType: 'uint256',
-        name: '_moment',
-        type: 'uint256',
-      },
-      {
         internalType: 'address',
         name: 'user',
         type: 'address',
@@ -555,6 +556,30 @@ export const MARKETPLACE_ABI = [
       },
     ],
     name: 'getOfferBalance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_moment',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'user',
+        type: 'address',
+      },
+    ],
+    name: 'getWithdrawableBalance',
     outputs: [
       {
         internalType: 'uint256',
