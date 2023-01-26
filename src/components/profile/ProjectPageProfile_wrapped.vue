0ecc1be85659a39096544740897ab562376a51e1
mosquito1717 <ymj08073@gmail.com>
2023-01-26 14:27:19 +0900

    fixed error of makeS3Path

--- src/components/profile/ProjectPageProfile_wrapped.vue
+++ src/components/profile/ProjectPageProfile_wrapped.vue
@@ -44,6 +44,8 @@ export default {
       return makeS3Path(path)
     },
     projectImagePath(projectImageUrl) {
+      console.log(projectImageUrl)
+      console.log(this.makeS3Path(projectImageUrl))
       return this.makeS3Path(projectImageUrl)
     },
   },
