34e08a2d2569204293f869fab9fa8adb26658d41
taeho_jeon <superduper8989@gmail.com>
2023-01-30 15:40:08 +0900

    CuratedCollection in detail components done

--- src/components/collection_card/CuratedCollection.vue
+++ src/components/collection_card/CuratedCollection.vue
@@ -31,18 +31,10 @@ export default {
   },
   props: {
     projects: {
-      type: Object,
-      default: null,
+      type: Array,
+      default: () => [],
     },
   },
-  methods: {
-    getTestNumber() {},
-  },
-  // methods: {
-  //     handleSlideClick(dataset)=>{
-  //         console.log(dataset.index, dataset.name)
-  //     }
-  // }
 }
 </script>
 
