43d9ecc2204d868d5052fda4e5ade22db7958a1e
mosquito1717 <ymj08073@gmail.com>
2023-02-02 09:34:10 +0900

    working on mint modal

--- src/components/modal/mint_steps/MintStep2.vue
+++ src/components/modal/mint_steps/MintStep2.vue
@@ -1,27 +1,33 @@
 <template>
-  <div>
-    <span>WHEN?</span>
-    <input
-      type="radio"
-      id="r1"
-      v-model="lazyMint"
-      value="0"
-      :disabled="project.policy === 1"
-      @change="onLazyMintChange"
-    />
-    <label for="r1">Mint Now</label>
-    <input
-      type="radio"
-      id="r2"
-      v-model="lazyMint"
-      value="1"
-      :disabled="project.policy === 1"
-      @change="onLazyMintChange"
-    />
-    <label for="r2">Lazy Mint</label>
+  <div class="container">
+    <div class="when">
+      <span>WHEN?</span>
+      <input
+        class="mint-now-radio"
+        type="radio"
+        id="r1"
+        v-model="lazyMint"
+        value="0"
+        :disabled="project.policy === 1"
+        @change="onLazyMintChange"
+      />
+      <label for="r1">Mint Now</label>
+      <input
+        class="lazy-mint-radio"
+        type="radio"
+        id="r2"
+        v-model="lazyMint"
+        value="1"
+        :disabled="project.policy === 1"
+        @change="onLazyMintChange"
+      />
+      <label for="r2">Lazy Mint</label>
+    </div>
+
     <div v-show="lazyMint == 1">
       <span>Price</span>
       <input
+        class="price-input"
         type="text"
         v-model="price"
         placeholder="Price in ETH"
@@ -60,3 +66,21 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+@import '../../../assets/scss/variables';
+.container {
+  text-align: left;
+  .when {
+    margin-bottom: 20px;
+    .mint-now-radio {
+      margin-left: 69px;
+    }
+    .lazy-mint-radio {
+      margin-left: 30px;
+    }
+  }
+  .price-input {
+    margin-left: 91px;
+  }
+}
+</style>
