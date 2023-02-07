45f89857b198e59d362865995e1c8dadb5ff70ac
mosquito1717 <ymj08073@gmail.com>
2023-02-07 13:38:43 +0900

    working on info component

--- src/components/util/Test.vue
+++ src/components/util/Test.vue
@@ -1,56 +1,32 @@
 <template>
   <div class="container">
     This is a test
-    <div class="box">click outside of this box</div>
+    <Info class="info" :words="words"></Info>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-
+import Info from '../util/Info.vue'
 export default {
   name: 'Test',
-  components: {},
+  components: {
+    Info,
+  },
   data() {
     return {
-      isSearchModalOpen: false,
+      words: 'hi this is captain speaking',
     }
   },
-  computed: {
-    ...mapState({
-      isModalOpen: state => state.menu.isModalOpen,
-    }),
-    isMobiel() {
-      return this.$isMobiel()
-    },
-  },
-  methods: {
-    toggleSearchModal() {
-      this.isSearchModalOpen = !this.isSearchModalOpen
-    },
-    setWidth() {
-      this.$store.commit('SET_INNER_WIDTH', window.innerWidth)
-    },
-    print() {
-      console.log('clicked outside')
-    },
-  },
-  created() {},
-  watch: {
-    isModalOepn() {
-      document.body.classList.toggle('prevent-scroll')
-    },
-  },
+  methods: {},
 }
 </script>
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
 
 .container {
-  padding-top: 70px;
-  .box {
-    width: 100%;
-    height: 30px;
-    background-color: red;
+  //padding-top: 70px;
+  margin-left: auto;
+  margin-right: auto;
+  .info {
   }
 }
 </style>
