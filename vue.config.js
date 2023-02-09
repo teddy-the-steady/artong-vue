2062163d8625c25617518b7546aeec11ca786f2b
teddy <hohoworld11@gmail.com>
2023-02-09 14:13:38 +0900

    added vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   runtimeCompiler: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'ko',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false,
+    },
+  },
 }
