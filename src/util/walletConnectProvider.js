a091830c094155a1be86b905f62c606f67bced0f
teddy <teddy.jeon@speedwagon.io>
2023-07-03 11:56:52 +0900

    walletconnect v2 migration

--- src/util/walletConnectProvider.js
+++ src/util/walletConnectProvider.js
@@ -3,31 +3,45 @@ import { EthereumProvider } from '@walletconnect/ethereum-provider'
 import { ethers } from 'ethers'
 
 class Provider {
+  METAMASK_ID =
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96'
+  RAINBOW_WALLET_ID =
+    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369'
+
+  MAINNET = 1
+  GOERLI = 5
+
   constructor() {
-    // this.provider = new WalletConnectProvider({
-    //   infuraId: process.env.VUE_APP_INFURA_ID,
-    //   qrcodeModalOptions: {
-    //     mobileLinks: ['metamask', 'rainbow'],
-    //   },
-    // })
-    this.provider = EthereumProvider.init({
+    EthereumProvider.init({
       projectId: process.env.VUE_APP_WALLETCONNECT_PROJECT_ID,
-      chains: [1],
+      chains: [this.MAINNET],
+      optionalChains: [this.GOERLI],
+      showQrModal: true,
+      qrModalOptions: {
+        explorerExcludedWalletIds: 'ALL',
+        explorerRecommendedWalletIds: [
+          this.METAMASK_ID,
+          this.RAINBOW_WALLET_ID,
+        ],
+      },
     })
-      .then(result => console.log(result))
+      .then(result => console.log((this.provider = result)))
       .catch(error => console.log(error))
   }
 
   async resetProvider() {
-    // this.provider = new WalletConnectProvider({
-    //   infuraId: process.env.VUE_APP_INFURA_ID,
-    //   qrcodeModalOptions: {
-    //     mobileLinks: ['metamask', 'rainbow'],
-    //   },
-    // })
     this.provider = await EthereumProvider.init({
       projectId: process.env.VUE_APP_WALLETCONNECT_PROJECT_ID,
-      chains: [1],
+      chains: [this.MAINNET],
+      optionalChains: [this.GOERLI],
+      showQrModal: true,
+      qrModalOptions: {
+        explorerExcludedWalletIds: 'ALL',
+        explorerRecommendedWalletIds: [
+          this.METAMASK_ID,
+          this.RAINBOW_WALLET_IDF,
+        ],
+      },
     })
   }
 
